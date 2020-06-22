82137926a3178bfa42a233526e83d0b2b381105a
Syizwan Jamalludin <syizwan.jamalludin@gmail.com>
2020-06-22 20:14:18 +0800

    finish section 10 - 11

---
+++ Section-10/util/database.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'Password2303!'
+
+});
+
+module.exports = pool.promise();
